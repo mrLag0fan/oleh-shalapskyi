20f38e336b1ca66f1ea5a905afb6008c09b3ecfc
Oleg Shalapsky <75427191+mrLag0fan@users.noreply.github.com>
2022-06-28 17:59:43 +0300

    HW_2 Done

---
+++ HW_2/src/main/java/com/epam/homework2/beans/Bean.java
@@ -0,0 +1,6 @@
+package com.epam.homework2.beans;
+
+public interface Bean {
+    public String getName();
+    public Integer getValue();
+}
---
+++ HW_2/src/main/java/com/epam/homework2/config/OtherConfig.java
@@ -0,0 +1,12 @@
+package com.epam.homework2.config;
+
+import com.epam.homework2.beans.BeanB;
+import com.epam.homework2.beans.BeanC;
+import com.epam.homework2.beans.BeanD;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan("com.epam.homework2.beans")
+public class OtherConfig {
+
+}
