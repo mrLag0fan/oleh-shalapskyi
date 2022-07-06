136b9bef7d111dc665d302bb79acf574f84a0f74
Oleg Shalapsky <75427191+mrLag0fan@users.noreply.github.com>
2022-07-06 16:53:11 +0300

    remove

--- HW-1/src/main/java/com/epam/spring/homework1/pet/Spider.java
+++
@@ -1,9 +0,0 @@
-package com.epam.spring.homework1.pet;
-
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Component;
-
-@Lazy
-@Component
-public class Spider implements Animal{
-}
