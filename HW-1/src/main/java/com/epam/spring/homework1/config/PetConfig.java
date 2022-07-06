cb5b3459a4d5ff0874afb8eeea02d686988e4bd9
Oleg Shalapsky <75427191+mrLag0fan@users.noreply.github.com>
2022-07-06 16:58:51 +0300

    HW_1 Fix

---
+++ HW-1/src/main/java/com/epam/spring/homework1/config/PetConfig.java
@@ -0,0 +1,29 @@
+package com.epam.spring.homework1.config;
+
+import com.epam.spring.homework1.pet.Cheetah;
+import com.epam.spring.homework1.pet.Spider;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan(
+        basePackages = "com.epam.spring.homework1.pet",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Spider.class))
+public class PetConfig {
+
+    @Bean
+    @Primary
+    public Cheetah cheetah() {
+        Cheetah res = new Cheetah();
+        System.out.println(res + " primary");
+        return res;
+    }
+
+    @Bean
+    @Qualifier
+    public Cheetah cheetah1() {
+        Cheetah res = new Cheetah();
+        System.out.println(res + " qualifier");
+        return res;
+    }
+}
