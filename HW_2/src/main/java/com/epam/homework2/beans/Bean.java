d19927a5cff3813f26e564a5bb467485899122ab
Oleg Shalapsky <75427191+mrLag0fan@users.noreply.github.com>
2022-07-08 14:20:24 +0300

    Delete HW_2 directory

--- HW_2/src/main/java/com/epam/homework2/beans/Bean.java
+++
@@ -1,7 +0,0 @@
-package com.epam.homework2.beans;
-
-public interface Bean {
-
-    String getName();
-    Integer getValue();
-}
--- HW_2/src/main/java/com/epam/homework2/config/OtherConfig.java
+++
@@ -1,9 +0,0 @@
-package com.epam.homework2.config;
-
-import org.springframework.context.annotation.*;
-
-@Configuration
-@ComponentScan("com.epam.homework2.beans")
-public class OtherConfig {
-
-}
