8251154b9a932874f4cf5da61d3fb1c7bd5d3e0b
Oleg Shalapsky <75427191+mrLag0fan@users.noreply.github.com>
2022-07-09 15:53:55 +0300

    refactored

--- HW-1/src/main/java/com/epam/spring/homework1/config/PetConfig.java
+++ HW-1/src/main/java/com/epam/spring/homework1/config/PetConfig.java
@@ -3,7 +3,11 @@ package com.epam.spring.homework1.config;
 import com.epam.spring.homework1.pet.Cheetah;
 import com.epam.spring.homework1.pet.Spider;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan(
